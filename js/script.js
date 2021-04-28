2ebc25fbff771242520916ab17b77db16dfb2c4b
Mattia Bocchino <pmattiab@live.it>
2021-04-28 18:24:06 +0200

    bonus 1 completato

--- js/script.js
+++ js/script.js
@@ -8,13 +8,17 @@ var app = new Vue(
         data: {
 
             // array dischi inizialmente vuoto
-            disks: []
+            disks: [],
+            genres: [],
+            disksFilter: "All"
         },
+
+        methods: {},
 
         // funzioni in mounted che partono dopo il caricamento dell'HTML
         mounted() {
 
-            // axios con API boolean
+            // axios con API boolean per riempire l'array dei dischi in data
             axios
                 .get("https://flynn.boolean.careers/exercises/api/array/music")
                 .then((response) => {
@@ -23,6 +27,21 @@ var app = new Vue(
 
                     // copio il risultato ottenuto nell'array dei dischi
                     this.disks = result.response;
+
+                    this.genres.push("All");
+
+                    // ciclo forEach su tutti gli oggetti dell'array disks
+                    this.disks.forEach(element => {
+
+                        // per pushare, se non già inclusi,
+                        // i generi nell'array genres in data
+                        if (!this.genres.includes(element.genre)) {
+                            this.genres.push(element.genre);
+                        }
+
+                    });
+
+                    console.log(this.genres);
                     
                 });
         }
